0a2b5da26cf2f8b5b44e33a89fb701fe67049dd2
hwangsunsoo <hwangsunsoo@gmail.com>
2018-10-11 22:58:56 +0900

    comment, csrf

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var cookieParser = require('cookie-parser');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -29,6 +30,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get( '/', function(req,res){
     res.send('first app!!!');
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var ProductsModel = require('../models/ProductsModel');
-
+var CommentsModel = require('../models/CommentsModel');
+var csrf = require('csurf');
+var csrfProtection = csrf({ cookie: true });
 
 router.get( '/', function(req,res){
     res.redirect('/admin/products');
@@ -15,37 +17,70 @@ router.get('/products', function(req,res){
     });
 });
 
-router.get('/products/write', function(req,res){
+router.get('/products/write', csrfProtection , function(req,res){
     //edit에서도 같은 form을 사용하므로 빈 변수( product )를 넣어서 에러를 피해준다
-    res.render( 'admin/form' , { product : "" }); 
+    res.render( 'admin/form' , { product : "", csrfToken : req.csrfToken() }); 
 });
 
-router.post('/products/write', function(req,res){
+// router.post('/products/write', function(req,res){
+//     var product = new ProductsModel({
+//         name : req.body.name,
+//         price : req.body.price,
+//         description : req.body.description,
+//     });
+//     product.save(function(err){
+//         res.redirect('/admin/products');
+//     });
+// });
+
+router.post('/products/write', csrfProtection, function(req,res){
     var product = new ProductsModel({
         name : req.body.name,
         price : req.body.price,
         description : req.body.description,
     });
-    product.save(function(err){
-        res.redirect('/admin/products');
-    });
+    // var validationError = product.validateSync();
+    // if(validationError){
+    //     res.send(validationError);
+    // }else{
+    //     product.save(function(err){
+    //         res.redirect('/admin/products');
+    //     });
+    // }
+    if(!product.validateSync()){
+        product.save(function(err){
+            res.redirect('/admin/products');
+        });
+    }else{
+        res.send('error');
+    }
 });
+
+// router.get('/products/detail/:id' , function(req, res){
+//     //url 에서 변수 값을 받아올떈 req.params.id 로 받아온다
+//     ProductsModel.findOne( { 'id' :  req.params.id } , function(err ,product){
+//         res.render('admin/productsDetail', { product : product });  
+//     });
+// });
 
 router.get('/products/detail/:id' , function(req, res){
     //url 에서 변수 값을 받아올떈 req.params.id 로 받아온다
     ProductsModel.findOne( { 'id' :  req.params.id } , function(err ,product){
-        res.render('admin/productsDetail', { product : product });  
+        //제품정보를 받고 그안에서 댓글을 받아온다.
+        CommentsModel.find({ product_id : req.params.id } , function(err, comments){
+            res.render('admin/productsDetail', { product: product , comments : comments });
+        });        
     });
 });
 
-router.get('/products/edit/:id' ,function(req, res){
+router.get('/products/edit/:id', csrfProtection, function(req, res){
     //기존에 폼에 value안에 값을 셋팅하기 위해 만든다.
     ProductsModel.findOne({ id : req.params.id } , function(err, product){
-        res.render('admin/form', { product : product });
+        res.render('admin/form', { product : product, csrfToken : req.csrfToken() });
     });
 });
 
-router.post('/products/edit/:id', function(req, res){
+router.post('/products/edit/:id', csrfProtection, function(req, res){
     //넣을 변수 값을 셋팅한다
     var query = {
         name : req.body.name,
@@ -54,14 +89,42 @@ router.post('/products/edit/:id', function(req, res){
     };
 
     //update의 첫번째 인자는 조건, 두번째 인자는 바뀔 값들
-    ProductsModel.update({ id : req.params.id }, { $set : query }, function(err){
-        res.redirect('/admin/products/detail/' + req.params.id ); //수정후 본래보던 상세페이지로 이동
-    });
+    // ProductsModel.update({ id : req.params.id }, { $set : query }, function(err){
+    //     res.redirect('/admin/products/detail/' + req.params.id ); //수정후 본래보던 상세페이지로 이동
+    // });
+    var products = new ProductsModel(query);
+    if(!products.validateSync()){
+        ProductsModel.update({ id : req.params.id }, { $set : query }, function(err){
+            res.redirect('/admin/products/detail/' + req.params.id ); //수정후 본래보던 상세페이지로 이동
+        });
+    }else{
+        res.send('error');
+    }
 });
 
 router.get('/products/delete/:id', function(req, res){
     ProductsModel.remove({ id : req.params.id }, function(err){
         res.redirect('/admin/products');
+    });
+});
+
+router.post('/products/ajax_comment/insert', function(req,res){
+    var comment = new CommentsModel({
+        content : req.body.content,
+        product_id : parseInt(req.body.product_id)
+    });
+    comment.save(function(err, comment){
+        res.json({
+            id : comment.id,
+            content : comment.content,
+            message : "success"
+        });
+    });
+});
+
+router.post('/products/ajax_comment/delete', function(req, res){
+    CommentsModel.remove({ id : req.body.comment_id } , function(err){
+        res.json({ message : "success" });
     });
 });
 
